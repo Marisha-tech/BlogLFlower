45974b4e94afec7692ecfe9e81aac423d0f7898b
karpova.m <34@3389.ru>
2022-01-28 17:38:32 +0700

    Главный роутинг

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {MainLayoutComponent} from "./shared/components/main-layout/main-layout.component";
+import {HomePageComponent} from "./home-page/home-page.component";
+import {PostPageComponent} from "./post-page/post-page.component";
+import {AdminModule} from "./admin/admin.module";
+import {SkillSectionComponent} from "./shared/sections/skill-section/skill-section.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},//для редиректа
+      {path: '', component: HomePageComponent},
+      {path: '', component: SkillSectionComponent},
+      {path: 'post/:id', component: PostPageComponent}
+    ]
+  },
+  {
+    // path: 'admin', loadChildren: './admin/admin.module#AdminModule'//устарело
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule)
+
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules//стратегия загрузки различных lazy-loading модулей
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
