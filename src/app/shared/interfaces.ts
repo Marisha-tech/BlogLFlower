ab020efbe27681124b4c3e4e5fc5e9c2d2b65d6d
karpova.m <34@3389.ru>
2022-01-31 13:03:00 +0700

    Интерфейс пользователя

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -1,4 +1,10 @@
 export interface User {
   email: string
   password: string
+  returnSecureToken?: boolean //в firebase в теле запроа должен быть returnSecureToken = true
+}
+
+export interface FbAuthResponse {
+  idToken: string
+  expiresIn: string //expiresIn - время жизни токена
 }
