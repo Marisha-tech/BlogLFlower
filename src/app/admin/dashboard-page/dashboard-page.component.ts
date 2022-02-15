9a1f19f86290f97bf6886974522f581cf6fb899b
karpova.m <34@3389.ru>
2022-02-15 12:23:22 +0700

    Список постов в админке

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,22 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from "../shared/services/auth.service";
+import {PostsServices} from "../../shared/posts.services";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+
+  posts: Post[] = []
+  pSub: Subscription | any //для отписки подписки
+  dSub: Subscription | any //для отписки от remove
+  searchStr = ''
 
   constructor(
-    private auth: AuthService
-  ) { }
+    private auth: AuthService,
+    private postsService: PostsServices,
+  ) {
+  }
 
   ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
+    if (this.dSub) {
+      this.dSub.unsubscribe()
+    }
   }
 
   // для проверки работает ли токен
   test() {
     console.log(this.auth.token)
+  }
+
+
+  remove(id: any) {
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id)
+    })
   }
 }
