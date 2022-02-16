e7051704262d1b5ffbc606b87ca3e978cb6ddd07
karpova.m <34@3389.ru>
2022-02-16 09:23:22 +0700

    Регистрация локализации

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import {NgModule, Provider} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatTabsModule} from "@angular/material/tabs";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from "@angular/common";
+import ruLocale from "@angular/common/locales/ru";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {PostPageComponent} from './post-page/post-page.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSliderModule} from "@angular/material/slider";
-import {MatTabsModule} from "@angular/material/tabs";
 import { SkillSectionComponent } from './shared/components/sections/skill-section/skill-section.component';
-import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {MatMenuTrigger} from "@angular/material/menu";
 import { PostComponent } from './shared/components/post/post.component';
 import { HeaderMainComponent } from './shared/components/headers/header-main/header-main.component';
 import {HeaderComponent} from "./shared/components/headers/header/header.component";
 import { FooterComponent } from './shared/components/footers/footer/footer.component';
 import {SharedModule} from "./shared/shared.module";
-import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {AuthInterceptor} from "./shared/auth.interceptor";
+
+//локализация для вывода даты на русском
+registerLocaleData(ruLocale, 'ru')
 
 const INTERCEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
