fd6e6ef8c196e827c4afb03d4223035f873ae3a5
karpova.m <34@3389.ru>
2022-02-16 16:35:39 +0700

    Добавление pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,22 @@ import {MatTabsModule} from "@angular/material/tabs";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {registerLocaleData} from "@angular/common";
 import ruLocale from "@angular/common/locales/ru";
+import {ServiceWorkerModule} from '@angular/service-worker';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {PostPageComponent} from './post-page/post-page.component';
-import { SkillSectionComponent } from './shared/components/sections/skill-section/skill-section.component';
+import {SkillSectionComponent} from './shared/components/sections/skill-section/skill-section.component';
 import {MatMenuTrigger} from "@angular/material/menu";
-import { PostComponent } from './shared/components/post/post.component';
-import { HeaderMainComponent } from './shared/components/headers/header-main/header-main.component';
+import {PostComponent} from './shared/components/post/post.component';
+import {HeaderMainComponent} from './shared/components/headers/header-main/header-main.component';
 import {HeaderComponent} from "./shared/components/headers/header/header.component";
-import { FooterComponent } from './shared/components/footers/footer/footer.component';
+import {FooterComponent} from './shared/components/footers/footer/footer.component';
 import {SharedModule} from "./shared/shared.module";
 import {AuthInterceptor} from "./shared/auth.interceptor";
+import {environment} from '../environments/environment';
 
 //локализация для вывода даты на русском
 registerLocaleData(ruLocale, 'ru')
@@ -51,12 +53,17 @@ const INTERCEPTOR_PROVIDER: Provider = {
     MatSliderModule,
     MatTabsModule,
     MatProgressBarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
     // MatMenuTrigger,
 
   ],
   providers: [INTERCEPTOR_PROVIDER],
-  exports: [
-  ],
+  exports: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
